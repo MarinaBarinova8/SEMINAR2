209dbbf6931c17618f93b5f3f055d3e23519de76
Marina Barinova <marina.alive888@yandex.ru>
2023-02-11 21:18:15 +0300

    Задача3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3.  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+ 
+
+ void WeekDay(int number)
+{
+    if(number > 5 && number <8 )
+Console.Write("This day of the week is  weekend");
+    else
+    Console.Write("This day of the week is not weekend");
+}
+
+Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+WeekDay(num);
