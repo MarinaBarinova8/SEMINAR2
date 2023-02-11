efa7d54897fdd867aad90c936387ef93bc089f63
Marina Barinova <marina.alive888@yandex.ru>
2023-02-11 20:48:55 +0300

    Задача1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1. Напишите программу, которая принимает за вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+int Digits (int number)
+{
+    int num1 = number / 10;
+    int num2 = num1 % 10;
+    return num2;
+}
+
+Console.Write("Input your three-digit number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+int result = Digits(user_number);
+Console.WriteLine($"Your number is {result}");
